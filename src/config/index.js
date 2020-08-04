900126139c708c27e88e707f071a6ef65c8a0904
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-08-03 22:33:56 -0300

    some adjustment

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080/categorias'
-  : 'https://reactflix-server.herokuapp.com/categorias';
+  ? 'http://localhost:8080'
+  : 'https://reactflix-server.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,14 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import useForm from '../../hooks/useForm';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
+
 
 
 function CadastroVideo() {
+
+  const history = useHistory();
+  const { handleChange, values } = useForm({
+    titulo: 'Video padrão',
+    url: 'https://www.youtube.com/watch?v=5vtw8vdc9wM',
+    categoria: 'Front End'
+
+    
+  });
+
     return(
       <PageDefault>
          <h1>
             Cadastro de Vídeo
          </h1>
+
+         <form onSubmit={(event) => {
+           event.preventDefault();
+
+           videosRepository.create({
+             titulo: '',
+             url: values.url,
+             categoriasId: 1,
+           })
+            .then(() => {
+              console.log('Cadastrou com sucesso!');
+              history.push('/');
+            });
+    
+         }}
+         >
+            <FormField
+            label="Titulo da Categoria"
+            name="titulo"
+            value={values.titulo}
+            onChange={handleChange}
+            />
+
+            <FormField
+            label="URL"
+            name="url"
+            value={values.url}
+            onChange={handleChange}
+            />
+
+            <FormField
+            label="Categoria"
+            name="categoria"
+            value={values.categoria }
+            onChange={handleChange}
+            />
+
+            <Button type="submit">
+          Cadastrar
+        </Button>
+         </form>
 
         <Link to="/cadastro/categoria">
             Cadastrar categoria
